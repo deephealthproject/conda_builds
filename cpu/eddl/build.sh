6bc2a7de98d4706d5ba3750d792bb41e467de325
simleo <simleo@crs4.it>
2021-01-21 09:54:16 +0100

    updated cpu/eddl recipe

--- cpu/eddl/build.sh
+++ cpu/eddl/build.sh
@@ -3,6 +3,9 @@
 mkdir build
 cd build
 cmake -DBUILD_EXAMPLES=OFF \
+      -DBUILD_SHARED_LIBS=ON \
+      -DBUILD_PROTOBUF=ON \
+      -DBUILD_HPC=OFF \
       -DBUILD_TESTS=OFF \
       -DCMAKE_INSTALL_PREFIX=${PREFIX} \
       -DBUILD_TARGET=CPU \
