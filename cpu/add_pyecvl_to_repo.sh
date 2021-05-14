0c329157a62397a05ec3acd64c6096ec56f8b0f9
simleo <simleo@crs4.it>
2021-05-14 09:48:28 +0200

    pyecvl test: run examples; do not include test script in docker img

--- cpu/add_pyecvl_to_repo.sh
+++ cpu/add_pyecvl_to_repo.sh
@@ -9,11 +9,3 @@ for v in 36 37 38; do
     echo "adding ${pkg}"
     docker run --rm pyecvl-conda bash -c "cat ${pkg}" > "${pkg_dir}/$(basename ${pkg})"
 done
-
-pyecvl_v=$(ls -1 "${pkg_dir}" | grep -m1 pyecvl | cut -d '-' -f 3)
-echo downloading tests for pyecvl ${pyecvl_v}
-wget -q https://github.com/deephealthproject/pyecvl/archive/${pyecvl_v}.tar.gz
-tar xf ${pyecvl_v}.tar.gz
-rm -rf tests
-mv pyecvl-${pyecvl_v}/tests .
-rm -rf pyecvl-${pyecvl_v}
