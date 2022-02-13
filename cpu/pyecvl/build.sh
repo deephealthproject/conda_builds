ae9ff74328458ccd25f85f4ac04c227e0c5c2afe
simleo <simleo@crs4.it>
2022-02-13 12:27:39 +0100

    update to pyecvl 1.1.0

--- cpu/pyecvl/build.sh
+++ cpu/pyecvl/build.sh
@@ -4,6 +4,6 @@ export ECVL_WITH_DICOM=ON
 export ECVL_WITH_OPENSLIDE=ON
 export ECVL_EDDL=ON
 
-export CPATH="${PREFIX}/include/eigen3:${CPATH}"
+export CPATH="${PREFIX}/include/eigen3:${PREFIX}/include/openslide:${CPATH}"
 
 ${PYTHON} setup.py install
